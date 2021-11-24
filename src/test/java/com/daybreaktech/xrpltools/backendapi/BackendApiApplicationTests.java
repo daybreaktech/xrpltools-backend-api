7a897c2bd7ea3a0c75f5dc00a070452d2457099d
Isaac Daniel Palomares <zero.cooldown@outlook.com>
2021-11-25 07:19:43 +0800

    Initial Commit

---
+++ src/test/java/com/daybreaktech/xrpltools/backendapi/BackendApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.daybreaktech.xrpltools.backendapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
